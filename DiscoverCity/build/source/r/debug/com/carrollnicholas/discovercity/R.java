04e1687d5e6d13176c850184cfbd6c92d8af2756
Nicholas <nicholasjosephcarroll@gmail.com>
2014-03-10 11:10:33 +0000

    temp

--- DiscoverCity/build/source/r/debug/com/carrollnicholas/discovercity/R.java
+++ DiscoverCity/build/source/r/debug/com/carrollnicholas/discovercity/R.java
@@ -8,11 +8,15 @@
 package com.carrollnicholas.discovercity;
 
 public final class R {
+    public static final class array {
+        public static final int updateInterval=0x7f050000;
+        public static final int updateIntervalValues=0x7f050001;
+    }
     public static final class attr {
     }
     public static final class dimen {
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int a_camera=0x7f020000;
@@ -20,20 +24,20 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int button=0x7f080001;
-        public static final int camera_preview=0x7f080000;
+        public static final int action_settings=0x7f0a0002;
+        public static final int button=0x7f0a0001;
+        public static final int camera_preview=0x7f0a0000;
     }
     public static final class layout {
         public static final int activity_camera=0x7f030000;
     }
     public static final class menu {
-        public static final int camera=0x7f070000;
+        public static final int camera=0x7f090000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050000;
-        public static final int app_name=0x7f050001;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f070000;
+        public static final int app_name=0x7f070001;
+        public static final int hello_world=0x7f070002;
     }
     public static final class style {
         /** 
@@ -44,9 +48,12 @@ public final class R {
  API 11 theme customizations can go here. 
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f080001;
+    }
+    public static final class xml {
+        public static final int preferences=0x7f040000;
     }
 }
