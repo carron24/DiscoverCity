d4da3df2a0cfb70653b479f3cad41246d1780ec1
Nicholas <nicholasjosephcarroll@gmail.com>
2014-03-17 15:34:02 +0000

    location services

--- DiscoverCity/src/com/carrollnicholas/discovercity/TagActivity.java
+++ DiscoverCity/src/com/carrollnicholas/discovercity/TagActivity.java
@@ -1,8 +1,12 @@
 package com.carrollnicholas.discovercity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
@@ -12,6 +16,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.File;
 
@@ -26,6 +31,16 @@ public class TagActivity extends ActionBarActivity {
     String descText;
     ImageView imageView;
     Bitmap picturePreview;
+
+    private static final long MINIMUM_DISTANCE_CHANGE_FOR_UPDATES = 1; // in Meters
+    private static final long MINIMUM_TIME_BETWEEN_UPDATES = 1000; // in Milliseconds
+
+    protected LocationManager locationManager;
+
+    Location location;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,6 +62,17 @@ public class TagActivity extends ActionBarActivity {
         }
 
 
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                MINIMUM_TIME_BETWEEN_UPDATES,
+                MINIMUM_DISTANCE_CHANGE_FOR_UPDATES,
+        new MyLocationListener()
+        );
+
+
+
 
 
         mButton.setOnClickListener(
@@ -59,12 +85,65 @@ public class TagActivity extends ActionBarActivity {
                         descText = mEdit2.getText().toString();
                         Log.v("Tag Text",tagText);
                         Log.v("Description Text", descText);
+                        showCurrentLocation();
+
                     }
                 });
 
 
     }
 
+    private void showCurrentLocation() {
+
+        MyLocationListener myLoc = new MyLocationListener();
+        if ( !locationManager.isProviderEnabled( LocationManager.GPS_PROVIDER ) ) {
+            locationManager.requestSingleUpdate(LocationManager.NETWORK_PROVIDER,myLoc,null);
+            location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        }
+        else{
+            locationManager.requestSingleUpdate(LocationManager.NETWORK_PROVIDER,myLoc,null);
+            location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        }
+
+        if(location != null){
+            String message = String.format(
+                    "Current Location \n Longitude: %1$s \n Latitude: %2$s",
+                    location.getLongitude(), location.getLatitude()
+            );
+            Toast.makeText(TagActivity.this, message,
+                    Toast.LENGTH_LONG).show();
+
+        }
+        else{
+            Toast.makeText(TagActivity.this, "location is null",
+                    Toast.LENGTH_LONG).show();
+        }
+
+    }
+private class MyLocationListener implements LocationListener {
+    public void onLocationChanged(Location location) {
+        String message = String.format(
+                "New Location \n Longitude: %1$s \n Latitude: %2$s",
+                location.getLongitude(), location.getLatitude()
+        );
+        Toast.makeText(TagActivity.this, message, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    public void onStatusChanged(String s, int i, Bundle b){
+
+    }
+
+}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
